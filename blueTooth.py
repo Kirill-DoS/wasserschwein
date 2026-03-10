6e545d592923038731f2d925038b99869579fbda
D64-78-73-76-64 <Ymnikdanila@yandex.ru>
2026-03-10 18:08:05 +0300

    BlueTooth

--- blueTooth.py
+++ blueTooth.py
@@ -1,20 +1,33 @@
 import serial
-import serial.tools.list_ports
 
-ports = serial.tools.list_ports.comports()
-for port in ports:
-     print(f"Порт: {port.device}, Описание: {port.description}")
-     
-ser = serial.Serial("COM3", 9600)
+# Настройки порта
+# В Ubuntu Bluetooth-устройства обычно висят на /dev/rfcomm0
+port = "/dev/rfcomm0" 
+baudrate = 9600 # Убедитесь, что на роботе такая же скорость
+command = ("M")
+speed = (100)
+try:
+    # Инициализация соединения
+    ser = serial.Serial(port, baudrate, timeout=1)
+    print(f"Подключено к {port}")
+    
+    while True:
+        # Отправка команды (например, 'F' - вперед)
 
+        if command.lower() == 'exit':
+            break
+            
+        # Кодируем строку в байты и отправляем
+        ser.write(bytes([ord(command), speed]))
+        
+        # Если робот что-то отвечает, читаем:
+     #    if ser.in_waiting > 0:
+     #        response = ser.readline().decode('utf-8').strip()
+     #        print(f"Робот ответил: {response}")
 
-# Send character 'S' to start the program
-ser.write(bytes([ord('M'), 200]))
-# ser.write('M200')
-# # Read line   
-# while True:
-#     bs = ser.readline()
-#     print(bs)
+    ser.close()
+    print("Соединение закрыто.")
 
-
+except Exception as e:
+    print(f"Ошибка: {e}")
 
